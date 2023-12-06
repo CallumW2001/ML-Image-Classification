4f514ae9dc1be07eb0c1c9e6530c5c93fa0b4e67
CallumW2021 <91595511+CallumW2021@users.noreply.github.com>
2023-12-06 11:57:44 +0000

    Add files via upload

--- Machine Learning - Image Classification Assignment.py
+++ Machine Learning - Image Classification Assignment.py
@@ -8,54 +8,130 @@ import matplotlib.pylab as plt
 import tensorflow as tf
 import os
 from PIL import Image
-
-
+import PIL
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder
 
 foldersCount = len(next(os.walk('dataset2/triple_mnist/train'))[1])
-foldersList = next(os.walk('dataset2/triple_mnist/train'))
+foldersTrain = next(os.walk('dataset2/triple_mnist/train'))
+foldersTest = next(os.walk('dataset2/triple_mnist/test'))
+
 print("Number of Folders:",foldersCount)
 
-path = os.path.basename(os.path.dirname('dataset2/triple_mnist/train/*/*'))
+#print("Folders List:",foldersTrain)
+#path = os.path.basename(os.path.dirname('dataset2/triple_mnist/train/*/*'))
 
 #Reading in Images
 
 Num_Images = glob.glob('dataset2/triple_mnist/train/*/*.png')
+Num_Images2 = glob.glob('dataset2/triple_mnist/test/*/*.png')
 
 #Display Image Shape
 
 img_cv2 = cv2.imread(Num_Images[0])
 print("Shape:", img_cv2.shape)
 
-grayImg = []
+
+#Convert RGB Images to Grayscale
+
+grayImgTrain = []
+grayImgTest = []
 
 
-for i in range(0, len(Num_Images)):
+for i in range(0, int(len(Num_Images))):
 
-    grayImg.append(Image.open(Num_Images[i]).convert('L'))
+    grayImg = (Image.open(Num_Images[i]).convert('L'))
+    grayImg = np.array(grayImg) / 255
+    grayImgTrain.append(grayImg)
 
+for i in range(0, int(len(Num_Images2))):
 
-grayImages = np.array(grayImg)
+    grayImg2 = (Image.open(Num_Images2[i]).convert('L'))
+    grayImg2 = np.array(grayImg2) / 255
+    grayImgTest.append(grayImg2)
 
-print("Shape of the grayscale images array:", grayImages.shape)
+grayImagesTrain = np.array(grayImgTrain)
+grayImagesTest = np.array(grayImgTest)
+
+average_shape = np.mean([img.shape for img in grayImagesTrain], axis=0)
+print("Average Shape of Images:", average_shape)
+
+#Output Sample Image
 
 fix, ax = plt.subplots(figsize = (8, 8))
 ax.axis('off')
-ax.set_title(foldersList[1][0])
-plt.imshow(grayImg[0], cmap='gray')
+ax.set_title(foldersTrain[1][0])
+plt.imshow(grayImagesTrain[0], cmap='gray')
 plt.show()
 
-vectorImg = []
+#Part 2
+
+
+vectorImgTrain = []
+vectorImgTest = []
 
 #Flatten Images into Vectors
 
-for i in range(0, len(Num_Images)):
+for i in range(0, len(grayImagesTrain)):
 
-    vectorImg.append(grayImages[i].flatten())
+    vectorImgTrain.append((grayImagesTrain[i].flatten()))
 
-vectorImages = np.array(vectorImg)
+for i in range(0, len(grayImagesTest)):
+    
+    vectorImgTest.append((grayImagesTest[i].flatten()))
 
-print(vectorImages.shape)
+#print('Min: %.3f, Max: %.3f' % (vectorImgTrain[5].min(), vectorImgTrain[5].max()))
+
+trainImages = np.array(vectorImgTrain)
+testImages = np.array(vectorImgTest)
+
+#print("Train Images:", trainImages)
+#print("Test Images:", testImages)
+
+y_train = []
+y_test = []
+
+
+print(trainImages.shape)
+print(testImages.shape)
+
+for i in range(0, len(foldersTrain[1])):
+    for y in range(0, 100):
+        y_train.append(int(foldersTrain[1][i]))
+
+for i in range(0, len(foldersTest[1])):
+    for y in range(0, 100):  
+        y_test.append(int(foldersTest[1][i]))
+
+YTrain = np.array(y_train)
+YTest = np.array(y_test)
+
+print("Y_Train:", YTrain)
+print("Y_Test:", YTest)
+
+#Logistic Regression Model
+
+# Create and train the Logistic Regression model
+
+model = LogisticRegression(max_iter=1000)
+model.fit(trainImages, YTrain)
+
+# Make predictions on the test set
+y_pred = model.predict(testImages)
+
+# Evaluate the model
+accuracy = metrics.accuracy_score(YTest, y_pred)
+print(f"Accuracy: {accuracy:.2f}")
+
+# Visualize some test images and their predictions
+fig, axes = plt.subplots(2, 5, figsize=(10, 4))
+for i, ax in enumerate(axes.flat):
+    ax.imshow(grayImagesTest[i], cmap='gray')  # Use grayImagesTest for visualization
+    ax.set_title(f"True: {YTest[i]}, Pred: {y_pred[i]}")
+    ax.axis('off')
+
+plt.show()
 
 print("End")
-
 
